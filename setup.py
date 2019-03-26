62d2e129785391e7c0d53f8c9623d1035b253d0a
avergengineer <danielandrewshare@gmail.com>
2019-03-26 23:57:52 +0000

    Added initial setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name='hetzner-telegram-bot',
+        description='A Telegram bot that will allow you to interface with your hetzner project',
+        version='0.1',
+        author='averagengineer',
+        packages=['requests'],
+        )
+
