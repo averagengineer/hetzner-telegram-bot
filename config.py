a9fc1c978be16da4697cdf7b9c1af1211b09960a
avergengineer <danielandrewshare@gmail.com>
2019-03-28 23:43:08 +0000

    Updated setup and created config

---
+++ config.py
@@ -0,0 +1,3 @@
+credentials = {
+        "API_TOKEN": "",
+        }
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
         name='hetzner-telegram-bot',
         description='A Telegram bot that will allow you to interface with your hetzner project',
         version='0.1',
         author='averagengineer',
+        license='MIT',
         packages=['requests'],
         )
 
